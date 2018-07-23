65e0ab6e7ba2485dbe24832f30aa642fe4cb3bd1
nmolanog <nmolanog@gmail.com>
2018-07-23 11:17:24 -0500

    example data set for the package

---
+++ R/data.R
@@ -0,0 +1,18 @@
+###following http://kbroman.org/pkg_primer/pages/data.html
+
+#' test_data data simulated for bayesuirt package tests
+#'
+#' data simulated for bayesuirt tests
+#'
+#' @docType data
+#'
+#' @usage data(test_data)
+#'
+#' @format A data.frame object
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(test_data)
+#' summary(test_data)
+"test_data"
